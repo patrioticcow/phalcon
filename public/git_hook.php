67fe15463c89f5ceced4352cf19e63a729381a1c
patrioticcow <cristi.catea@gmail.com>
2014-04-16 13:45:32 -0700

    .

--- public/git_hook.php
+++ public/git_hook.php
@@ -1,11 +1,3 @@
 <?php
 
-$LOCAL_ROOT      = "/usr/share/nginx/html/phalcon";
-$LOCAL_REPO_NAME = "phalcon";
-$LOCAL_REPO      = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO     = "git@github.com:patrioticcow/phalcon.git";
-$DESIRED_BRANCH  = "dev";
-
-echo shell_exec("cd {$LOCAL_ROOT} && git pull");
-
-die("done " . mktime());
+shell_exec("cd /usr/share/nginx/html/phalcon && git pull");
